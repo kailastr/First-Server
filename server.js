d93a1f986167f598863d16798e03008c6db6a2bb
kailastr <mailtokailastr@gmail.com>
2023-09-04 01:54:09 +0530

    Installed 3rd party packages(nodemon)

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const http = require('http');
+const http = require('http'); // this is a package import
 
-const port = 8081;
+const port = 2255; //created a local port number
 
 http.createServer((request, Response) => {
     Response.writeHead(200, { 'Content-Type': 'text/html' });
-    Response.write("<h1>Hello, this is from the server</h1>");
+    Response.write("<h1>Hello, this is from My first server</h1>");
+    Response.write("<p>Hey user How is your experience in this server ??");
     Response.end();
 
-}).listen(port, () => {
+}).listen(port, () => { //callback function
     console.log(`Nodejs server started on port ${port}`);
 });
 
-//http://localhost:8081
+//http://localhost:2255
 //to start the server we should have to write "node server.js" in the terminal everytime we run this server. Instead we could do it in the JSON file's script section and type npm start
